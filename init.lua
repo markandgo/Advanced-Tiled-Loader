1e4279061dd1911ee445ea9d8cdefae4541e5a72
Minh Ngo <mkngo87@gmail.com>
2013-08-01 01:09:22 -0400

    Tidy up init.lua

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@ TILED_LOADER_PATH  = TILED_LOADER_PATH or (...):match('^.+[%.\\/]') or ''
 
 -- Return the classes in a table
 return {
-    Map = require(TILED_LOADER_PATH  .. "Map"),
-    TileLayer = require(TILED_LOADER_PATH  .. "TileLayer"),
-    Tile = require(TILED_LOADER_PATH  .. "Tile"),
-    TileSet = require(TILED_LOADER_PATH  .. "TileSet"),
-    Object = require(TILED_LOADER_PATH  .. "Object"),
-    ObjectLayer = require(TILED_LOADER_PATH  .. "ObjectLayer"),
-    Loader = require(TILED_LOADER_PATH  .. "Loader"),
-    Grid = require(TILED_LOADER_PATH .. "Grid"),
+	Map        = require(TILED_LOADER_PATH  .. "Map"),
+	TileLayer  = require(TILED_LOADER_PATH  .. "TileLayer"),
+	Tile       = require(TILED_LOADER_PATH  .. "Tile"),
+	TileSet    = require(TILED_LOADER_PATH  .. "TileSet"),
+	Object     = require(TILED_LOADER_PATH  .. "Object"),
+	ObjectLayer= require(TILED_LOADER_PATH  .. "ObjectLayer"),
+	Loader     = require(TILED_LOADER_PATH  .. "Loader"),
+	Grid       = require(TILED_LOADER_PATH .. "Grid"),
 }
